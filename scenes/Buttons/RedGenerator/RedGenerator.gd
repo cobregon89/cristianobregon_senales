f75004a84b5a25fdb91a83d43f9d0fbabfdb5d26
cobregon89 <62080655+cobregon89@users.noreply.github.com>
2020-04-21 12:25:21 -0300

    1

---
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+
+
+func _ready():
+	pass 
+
+
+func _on_RedGenerator_button_down():
+	
+	pass
---
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+
+
+func _ready():
+	pass 
+
+
+func _on_VioletGenerator_button_down():
+	
+	pass
