c9e576f1d98f5c280a856c09598c22dee15caa7d
cobregon89 <62080655+cobregon89@users.noreply.github.com>
2020-04-21 17:26:12 -0300

    2

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,10 +1,15 @@
 extends TextureButton
 
+var instance_block_red = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 func _ready():
 	pass 
 
+func generate_block():
+	var real_instance_block_red = instance_block_red.instance()
+	add_child(real_instance_block_red)
+	pass
 
 func _on_RedGenerator_button_down():
-	
+	generate_block()
 	pass
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,10 +1,15 @@
 extends TextureButton
 
+var instance_block_violet = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
 func _ready():
 	pass 
 
+func generate_block():
+	var real_instance_block_violet = instance_block_violet.instance()
+	add_child(real_instance_block_violet)
+	pass
 
 func _on_VioletGenerator_button_down():
-	
+	generate_block()
 	pass
