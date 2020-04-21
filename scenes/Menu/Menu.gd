cfaae42110192a7ae9a9773c834db1455a796949
cobregon89 <62080655+cobregon89@users.noreply.github.com>
2020-04-21 19:33:36 -0300

    7

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -4,13 +4,17 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 
-func count_new_instance():
-	instancesCount = instancesCount + 1
-	$CountLabel.text = "text: " + String (instancesCount)
-	pass
 
 func _ready():
+	$RedGenerator.connect("button_down",self,"count_new_instance")
+	$VioletGenerator.connect("button_down",self,"count_new_instance")
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
+	
+func count_new_instance():
+	instancesCount = instancesCount + 1
+	$CountLabel.text = INSTANCES_TEXT + String (instancesCount)
+	pass
+
